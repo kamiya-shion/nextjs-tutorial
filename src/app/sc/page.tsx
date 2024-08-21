fa5396664bd04b407c943e41280fedf442fe4406
kamiya <nanahumi0928@gmail.com>
2024-08-22 08:42:37 +0900

    サーバーコンポーネント・クライアントコンポーネント

---
+++ src/app/sc/page.tsx
@@ -0,0 +1,7 @@
+const ServerComponent = () => {
+  // コンソールには出力されずNext.jsが起動しているサーバー上で出力される
+  console.log("Sever Component");
+  return <div>ServerComponent</div>;
+};
+
+export default ServerComponent;
